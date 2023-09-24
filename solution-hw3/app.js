830e78792da4170f7621651273d1b9aeea6faf4d
Paula Shin <paulashin15@gmail.com>
2023-09-24 19:22:34 -0400

    added js file

---
+++ solution-hw3/app.js
@@ -0,0 +1,20 @@
+let originalGlaze = {glaze: 'Keep original', glazingPrice: 0}
+let sugarGlaze = {'Sugar milk', glazingPrice: 0}
+let vanillaGlaze = {glaze: 'Vanilla milk', glazingPrice: 0.5}
+let chocolateGlaze = {glaze: 'Double chocolate', glazingPrice: 1.5}
+
+// 
+let basePrice = 2.49;
+let something = document.getElementById("")
+
+function priceFunction(){
+    something.value = "Keep_Orig"
+    final_price = (basePrice + glazingPrice) * packPrice
+    something.innerHTML = final_price
+}
+
+for (let i = 0; i < glazing.length; i++) {
+    // iterate through each 
+}
+
+// add to the UI
