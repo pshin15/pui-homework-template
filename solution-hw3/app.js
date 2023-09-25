53c5b69f413540d591033495b91e662bf1ca3246
Paula Shin <paulashin15@gmail.com>
2023-09-25 15:15:06 -0400

    added drop down options with objects

--- solution-hw3/app.js
+++ solution-hw3/app.js
@@ -1,20 +1,85 @@
-let originalGlaze = {glaze: 'Keep original', glazingPrice: 0}
-let sugarGlaze = {'Sugar milk', glazingPrice: 0}
-let vanillaGlaze = {glaze: 'Vanilla milk', glazingPrice: 0.5}
-let chocolateGlaze = {glaze: 'Double chocolate', glazingPrice: 1.5}
-
-// 
-let basePrice = 2.49;
-let something = document.getElementById("")
-
-function priceFunction(){
-    something.value = "Keep_Orig"
-    final_price = (basePrice + glazingPrice) * packPrice
-    something.innerHTML = final_price
+// object for  price adaptations based on glazing option
+let glazingOptions = {
+    original: {
+        price: 0,
+        displayname: "Keep original"
+    },
+    sugarmilk: {
+        price: 0,
+        displayname: "Sugar milk"
+    },
+    vanillamilk: {
+        price: 0.5,
+        displayname: "Vanilla milk"
+    },
+    doublechocolate: {
+        price: 1.5,
+        displayname: "Double chocolate"
+    }
 }
 
-for (let i = 0; i < glazing.length; i++) {
-    // iterate through each 
+let packOptions = {
+    1: {
+        price: 1, 
+        displayname: "1"
+    },
+    3: {
+        price: 3, 
+        displayname: "3"
+    },
+    6: {
+        price: 6, 
+        displayname: "6"
+    },
+    12: {
+        price: 12, 
+        displayname: "12"
+    }
+}
+
+// glazingOptions.sugarmilk.price;
+
+let basePrice = 2.49;
+let glazingDropdown = document.getElementById("glazing-dropdown");
+let packDropdown = document.getElementById("pack-dropdown");
+
+//loop through the object. for every glzing option, we will create an <option> element and add to <select>
+const optionarray = Object.entries(glazingOptions);
+
+for (element of optionarray){
+    console.log(element[1]);
+    let newoption = document.createElement('option');
+    let display = document.createTextNode(element[1].displayname);
+    newoption.appendChild(display);
+    newoption.setAttribute('value', element[0]);
+    glazingDropdown.appendChild(newoption);
+}
+
+const packarray = Object.entries(packOptions);
+
+for (element of packarray){
+    console.log(element[1]);
+    let newpackoption = document.createElement('option');
+    let display = document.createTextNode(element[1].displayname);
+    newpackoption.appendChild(display);
+    newpackoption.setAttribute('value', element[0]);
+    packDropdown.appendChild(newpackoption);
+}
+
+
+function recompute(){
+    console.log("clicked");
+
+}
+
+function calculatePrice(){
+    final_price = (basePrice + glazingPrice) * packPrice
 }
 
 // add to the UI
+// let option = document.createElement('option');
+// option.innerHTML = glazingPriceTotal;
+// option.text = glazingPriceTotal;
+// selectElement.add(option);
+
+// selectElement.addEventListener('change', onSelectValueChange);
