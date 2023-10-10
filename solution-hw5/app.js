26bfedaa69ad77bcd022b808ded8420ac61ef57e
Paula Shin <paulashin15@gmail.com>
2023-10-10 17:07:30 -0400

    initial commit

--- solution-hw5/app.js
+++ solution-hw5/app.js
@@ -1,110 +1,3 @@
-// object for price adaptations based on glazing option
-let glazingOptions = {
-    original: {
-        price: 0,
-        displayname: "Keep original"
-    },
-    sugarmilk: {
-        price: 0,
-        displayname: "Sugar milk"
-    },
-    vanillamilk: {
-        price: 0.5,
-        displayname: "Vanilla milk"
-    },
-    doublechocolate: {
-        price: 1.5,
-        displayname: "Double chocolate"
-    }
-}
-// object for price adaptations based on pack sizes option
-let packOptions = {
-    1: {
-        priceadapt: 1, 
-        displayname: "1"
-    },
-    3: {
-        priceadapt: 3, 
-        displayname: "3"
-    },
-    6: {
-        priceadapt: 5, 
-        displayname: "6"
-    },
-    12: {
-        priceadapt: 10, 
-        displayname: "12"
-    }
-}
-
-let basePrice = 2.49;
-let selectedGlazingOption = document.getElementById("glazing-dropdown");
-let selectedPackSizeOption = document.getElementById("pack-dropdown");
-
-let totalprice = document.querySelector(".checkout");
-
-//loop through the object. for every glzing option, we will create an <option> element and add to <select>
-const optionarray = Object.entries(glazingOptions);
-
-for (element of optionarray){
-    // console.log(element[1]);
-    let newoption = document.createElement('option');
-    let display = document.createTextNode(element[1].displayname);
-    newoption.appendChild(display);
-    newoption.setAttribute('value', element[0]);
-    selectedGlazingOption.appendChild(newoption);
-}
-
-const packarray = Object.entries(packOptions);
-
-for (element of packarray){
-    // console.log(element[1]);
-    let newpackoption = document.createElement('option');
-    let display = document.createTextNode(element[1].displayname);
-    newpackoption.appendChild(display);
-    newpackoption.setAttribute('value', element[0]);
-    selectedPackSizeOption.appendChild(newpackoption);
-}
-
-let glazingoptionprice = glazingOptions[selectedGlazingOption];
-let packsizemultiplier = packOptions[selectedPackSizeOption];
-
-
-function calculatePrice(){
-    let selectedGlazing = selectedGlazingOption.value;
-    let selectedPackSize = selectedPackSizeOption.value;
-
-    let glazingPrice = glazingOptions[selectedGlazing];
-    let packSize = packOptions[selectedPackSize];
-
-    console.log("clicked");
-    final_price = (basePrice + glazingPrice.price) * packSize.priceadapt;
-    console.log(final_price);
-
-    totalprice.textContent = `$${final_price.toFixed(2)}`;
-}
-
-const queryString = window.location.search;
-const params = new URLSearchParams(queryString);
-const rollType = params.get(`roll`);
-
-// shows respective heading for each product page
-let heading = document.querySelector('#heading-title');
-heading.textContent = rolls[rollType].name;
-console.log(heading);
-
-// shows respective image for each product page
-let rollImage = rolls[rollType].imageFile;
-let productImage = document.querySelector('.product-detail-img');
-productImage.src = '../assets/products/' + rollImage;
-console.log(productImage.src); 
-
-// shows respective price for each product page
-let productPrice = document.querySelector('.checkout');
-productPrice.textContent = rolls[rollType].basePrice;
-console.log(productPrice);
-
-
 cart = [];
 
 class Roll {
@@ -114,41 +7,38 @@ class Roll {
         this.size = packSize;
         this.basePrice = basePrice;
     }
+
+
+
+    function calculatePrice(){
+        let selectedGlazing = selectedGlazingOption.value;
+        let selectedPackSize = selectedPackSizeOption.value;
+
+        let glazingPrice = glazingOptions[selectedGlazing];
+        let packSize = packOptions[selectedPackSize];
+
+        console.log("clicked");
+        final_price = (basePrice + glazingPrice.price) * packSize.priceadapt;
+        console.log(final_price);
+
+        totalprice.textContent = `$${final_price.toFixed(2)}`;
+    }
+
 }
 
 // 4 new Roll objects
-let original = {
-    type: "Original",
-    glazing: "Sugar Milk",
-    packSize: 1,
-    calculatedPrice: 2.49
-}
+let original = new Roll("Original", "Sugar Milk", 1, 2.49); 
+let walnut = new Roll("Walnut", "Vanilla Milk", 12, 39.9); 
+let raisin = new Roll("Raisin", "Sugar Milk", 3, 8.97);
+let apple = new Roll("Apple", "Original", 3, 10.47);
 
-let walnut = {
-    type: "Walnut",
-    glazing: "Vanilla Milk",
-    packSize: 12,
-    calculatedPrice: 39.9
-}
-
-let raisin = {
-    type: "Raisin",
-    glazing: "Sugar Milk",
-    packSize: 3,
-    calculatedPrice: 8.97
-}
-
-let apple = {
-    type: "Apple",
-    glazing: "Original",
-    packSize: 3,
-    calculatedPrice: 10.47
-}
 
 function addtoCart() {
-    let rollGlazing = selectedGlazingOption.options[selectedGlazingOption.selectedIndex].text;
-    let packSize = selectedPackSizeOption.options[selectedPackSizeOption.selectedIndex].text;
     const rollInstance = new Roll(rollType, rollGlazing, packSize, basePrice);
     cart.push(rollInstance);
     console.log(cart);
+}
+
+function displayCart(Roll) {
+
 }
